1043cc72c12b6f76fc436e69ec095b4ec4e1869a
Aswin krishna <85611393+jr-boney@users.noreply.github.com>
2024-06-07 13:23:36 +0530

    Update App.js

--- Learn React/App.js
+++ Learn React/App.js
@@ -1,12 +1,18 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Main from "./components/Main"
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
+};
 
-export default function App() {
-    return (
-        <div className="container">
-            <Navbar />
-            <Main />
-        </div>
-    )
-}
+
+
+Items.defaultProps = {
+  quantity: 0
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <Items />
+  }
+};
